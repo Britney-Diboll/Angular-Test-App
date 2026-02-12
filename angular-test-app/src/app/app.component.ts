b6bffaf3366cb46ddeddb1611a32262c0ec6de94
Diboll <bdiboll@proforma.com>
2026-02-12 15:29:54 -0500

    wip

--- angular-test-app/src/app/app.component.ts
+++ angular-test-app/src/app/app.component.ts
@@ -11,14 +11,13 @@ export class AppComponent {
     title = 'angular-test-app';
     classMembers: string[] = [];
     showTitle: boolean = true;
-    myModel: object = {FirstName: 'Britney'};
+    myModel: any = {};
 
     constructor() { }
 
     ngOnInit() {
         this.setClassMembers();
         this.showTitle = false;
-        debugger;
     }
 
     doSomething() {
@@ -26,6 +25,36 @@ export class AppComponent {
     }
 
     setClassMembers() {
-        this.classMembers = ['My', 'Zac', 'Zach' ];
+        this.classMembers = ['My', 'Zac', 'Zach'];
     }
+
+    validateName(event: any) : void {
+        // let res = event.target.value;
+        // if (res.length < 2) {
+        //     alert('Name must be greater than 2 characters, please try again.');
+        // } else {
+        //     alert('Name is valid!');
+        // }
+
+        // OR
+
+        if (this.myModel.FirstName.length < 2) {
+            alert('Name must be greater than 2 characters, please try again.');
+        } else {
+            alert('Name is valid!');
+        }
+    }
+
+
+
+    classFunction() : string{
+        // debugger;
+        if (this.myModel.FirstName?.length < 2) {
+            return 'bd-color-red';
+        } else {
+            return 'bd-color-green ';
+        }
+    }
+
+
 }
